4f6634819d942dd081c24464c41bbda22f8c0b81
Sanjaya Sahu <sanjaya1792000@gmail.com>
2023-12-15 18:41:14 +0530

    update 2

--- src/main/java/com/gullycric/controller/CricController.java
+++ src/main/java/com/gullycric/controller/CricController.java
@@ -5,7 +5,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.gullycric.model.Team;
 import com.gullycric.service.ITeamService;
 
 @Controller
@@ -13,9 +17,22 @@ public class CricController {
 	@Autowired
 	private ITeamService teamService;
 
-	@GetMapping("/team")
+	@GetMapping("/")
 	private String team(Map<String, Object> map) {
 		map.put("teams", teamService.getAllTeams());
 		return "team";
+	}
+
+	@PostMapping("/")
+	private String teamPost(@ModelAttribute Team team, Map<String, Object> map, RedirectAttributes attributes) {
+		map.put("teams", teamService.getAllTeams());
+
+		if (teamService.addTeam(team)) {
+			attributes.addAttribute("success", "Team Added Successfully.");
+		} else {
+			attributes.addAttribute("failure", "Unable to add Team!");
+		}
+		
+		return "/";
 	}
 }
